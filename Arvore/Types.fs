a31f371f2baa9b904e4c70afafc6eab953caa689
Leo Silva <alleqs@bol.com.br>
2024-08-26 08:20:45 -0400

    primeiro push

---
+++ Arvore/Types.fs
@@ -0,0 +1,62 @@
+﻿module Types
+
+//Alias
+type Date = System.DateTime
+type NF = string
+type PostoFiscal = string
+
+type Modal = 
+    | Aqua
+    | Aereo
+    | Rodo
+    | Multimodal
+
+type DocumentoBase =
+    | MDFComposto of MDFComposto //MDF áqua ou aéreo
+    | MDFSimples of MDFSimples //MDF rodo
+    | CL of CL
+
+and CT = {
+    CNPJEmitente: int
+    RSEmitente: string
+    Modal: Modal
+    DocsReferenciados: DocsReferenciadosCT
+}
+
+and DocsReferenciadosCT =
+    | NFs of NF[]
+    | CT of CT
+
+and MDFComposto = {
+    PF: PostoFiscal
+    DataApresentacao: Date
+    CNPJEmitente: int
+    RSEmitente: string
+    Modal: Modal
+    DocsReferenciados: DocsReferenciadosMDFComposto
+}
+
+and DocsReferenciadosMDFComposto =
+    | NFs of NF[]
+    | CTs of CT[]
+    | ``NFs e CTs`` of NF[] * CT[]
+
+and MDFSimples = {
+    PF: PostoFiscal
+    DataApresentacao: Date
+    CNPJEmitente: int
+    RSEmitente: string
+    Modal: Modal
+    DocsReferenciados: DocsReferenciadosMDFSimples
+}
+
+and DocsReferenciadosMDFSimples =
+    | NFs of NF[]
+    | CTs of CT[]
+
+and CL = {
+    PF: PostoFiscal
+    DataApresentacao: Date
+    NFs: NF[]
+}
+
